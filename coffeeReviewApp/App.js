169c7af4f466764f4b609ea811d791038ebb9e68
sawaizmirza <m_sawaiz1791@ymail.com>
2021-02-24 23:43:15 +0000

    screens added

--- coffeeReviewApp/App.js
+++ coffeeReviewApp/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Home from './screens/Home';
+import Restaurant from './screens/Restaurant';
+import Login from './screens/Login';
+
+const Stack = createStackNavigator();
 
 const App = () => {
+  const [user, setUser] = useState({});
+
   return (
-    <SafeAreaView>
-      <Text>Coffee Review App</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {user ? (
+          <>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Restaurant" component={Restaurant} />
+          </>
+        ) : (
+          <Stack.Screen name="Login" component={Login} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
